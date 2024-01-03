2c99e4e9083e1c5d0c6dd01d310aeef28248abf3
gabrieljaguiar <gja.gbrl@gmail.com>
2024-01-03 14:27:04 -0500

    fixes

--- drift_detectors/__init__.py
+++ drift_detectors/__init__.py
@@ -1,3 +1,3 @@
 from .no_drift import NoDrift
-from .multi_class_detector import MultiDetector
+from .multi_class_detector import MultiDetector, InformedDrift
 from .ground_truth import TruthDetector
