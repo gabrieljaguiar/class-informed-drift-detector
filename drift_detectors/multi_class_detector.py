a0c6fbe0ba653aa61c15b6f0e24915bd05e5137d
gabrieljaguiar <gja.gbrl@gmail.com>
2024-01-04 08:57:29 -0500

    drift_detector

--- drift_detectors/multi_class_detector.py
+++ drift_detectors/multi_class_detector.py
@@ -20,26 +20,41 @@ class InformedDrift(MultiDetector):
         self, n_classes: int, window_size: int = 100, windows_to_train: int = 10
     ):
         super().__init__()
-        self.classifiers = {key: OneClassSVM() for key in range(0, n_classes)}
+        self.classifiers = {}
         self.windows = {
             key: collections.deque(maxlen=window_size) for key in range(0, n_classes)
         }
         self.current_concept = {key: [] for key in range(0, n_classes)}
         self.window_size = window_size
+        self.windows_to_train = windows_to_train
 
     def update(self, x: np.array, y: int):
         x["class"] = y
         self.windows[y].append(x)
         if len(self.windows[y]) == self.windows[y].maxlen:
+            #print (len(self.windows[y]))
             df = pd.DataFrame(self.windows[y])
             mfe = MFE(groups=["general", "statistical"], summary=["mean"])
             mfe.fit(df.iloc[:, :-1].to_numpy(), df.iloc[:, -1].to_numpy())
             ft = mfe.extract()
             ft = dict(zip(ft[0], ft[1]))
             self.current_concept[y].append(ft)
+            self.windows[y].clear()
+            #print (self.current_concept[y])
             # extract features
             # predict if there is a drift in that class or not
             # if not add this features to the current concept of that class
             # if drift detected  raise drift alarm, classes affected == y, reset one class classifier plus reset current concept
-            pass
+            #pass
+            if (self.classifiers.get(y, None)):
+                array_to_predict = np.array(list(self.current_concept[y][-1].values()))
+                array_to_predict = array_to_predict[~np.isnan(array_to_predict)].reshape(1,-1)
+                #print (array_to_predict)
+                print(self.classifiers[y].predict(array_to_predict))
+            if (len(self.current_concept[y]) > self.windows_to_train): #and (self.classifiers.get(y, None) == None):
+                self.classifiers[y] = OneClassSVM()
+                df = pd.DataFrame(self.current_concept[y]).dropna(axis=1, inplace=False)
+                self.classifiers[y].fit(df)
+                del df
+        
         self._drift_detected = False
